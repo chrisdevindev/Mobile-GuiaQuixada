bf6cd98cd871f0e43a4770909e03ad9621eab1bf
Roberlan Carvalho <roberlan.carvalho@gmail.com>
2021-07-10 20:18:12 -0300

    criação de página de login

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,19 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Login from "./pages/Home";
-import ScannerCode from "./pages/Scanner/Scanner";
 
-const { Navigator } = createStackNavigator();
+import Login from "./pages/Home";
+import ScannerCode from "./pages/Scanner";
+
+const { Navigator, Screen } = createStackNavigator();
+const Stack = createStackNavigator();
 
 function AppStack() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Scanner" component={ScannerCode} />
+        <Screen name="Login" component={Login} />
+        <Screen name="Scanner" component={ScannerCode} />
       </Navigator>
     </NavigationContainer>
   );
